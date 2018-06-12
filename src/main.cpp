3b9708f8ef35706b83991df716c734fc1432817d
jjwall <jacob.wallace@utexas.edu>
2018-06-11 20:55:18 -0500

    Set up game server tracking number of connections and port number

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
 
+using websocketpp::connection_hdl;
 using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 using websocketpp::lib::bind;
@@ -12,41 +13,69 @@ typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
 typedef websocketpp::client<websocketpp::config::asio_client> client;
 typedef websocketpp::server<websocketpp::config::asio> server;
 
-void on_message(websocketpp::connection_hdl, server::message_ptr msg) {
+class game_server
+{
+public:
+    static void on_message(connection_hdl, server::message_ptr msg)
+    {
         std::cout << msg->get_payload() << std::endl;
-}
+    }
+    static void on_open(connection_hdl hdl)
+    {
+        game_server::connections++;
+        std::cout << "We gained a connection: now we have " << game_server::connections << std::endl;
+    }
+    void on_close(connection_hdl hdl)
+    {
+        game_server::connections--;
+        std::cout << "We lost a connection: now we have " << game_server::connections << std::endl;
+    }
+    static int connections;
+    static int port;
+};
 
-void on_open(client* c, websocketpp::connection_hdl hdl) {
-    std::string msg = "Hi from the C++!";
-    c->send(hdl,msg,websocketpp::frame::opcode::text);
-    //c->close(hdl, websocketpp::close::status::normal,"");
-    //c->get_alog().write(websocketpp::log::alevel::app, "Sent Message: "+msg);
-}
+class game_client
+{
+public:
+    static void on_open(client* c, connection_hdl hdl)
+    {
+        std::string msg = "Hi from the C++!";
+        c->send(hdl,msg,websocketpp::frame::opcode::text);
+        //c->close(hdl, websocketpp::close::status::normal,"");
+        //c->get_alog().write(websocketpp::log::alevel::app, "Sent Message: "+msg);
+    }
+};
 
-int main(int argc, char* argv[]) {
+int game_server::connections = 0;
+int game_server::port;
+
+int main(int argc, char* argv[])
+{
     server websocket_server;
     client c;
     
-    int PORT = std::atoi(argv[1]);
+    game_server::port = std::atoi(argv[1]);
 
-    websocket_server.set_message_handler(&on_message);
+    websocket_server.set_message_handler(&game_server::on_message);
+    websocket_server.set_open_handler(&game_server::on_open);
     websocket_server.set_access_channels(websocketpp::log::alevel::all);
     websocket_server.set_error_channels(websocketpp::log::elevel::all);
 
     websocket_server.init_asio();
-    websocket_server.listen(PORT);
+    websocket_server.listen(game_server::port);
     websocket_server.start_accept();
 
-    std::cout << "Listening for connections on port " << PORT << std::endl;
+    std::cout << "Listening for connections on port " << game_server::port << std::endl;
 
     // attempt connection to lobby server
     std::string uri = "ws://localhost:8080";
 
-    try {
+    try
+    {
         // Set logging to be pretty verbose (everything except message payloads)
         //c.set_access_channels(websocketpp::log::alevel::all);
         //c.clear_access_channels(websocketpp::log::alevel::frame_payload);
-        c.set_open_handler(bind(&on_open,&c,::_1));
+        c.set_open_handler(bind(&game_client::on_open,&c,::_1));
 
         // Initialize ASIO
         c.init_asio();
@@ -57,7 +86,8 @@ int main(int argc, char* argv[]) {
         websocketpp::lib::error_code ec;
         client::connection_ptr con = c.get_connection(uri, ec);
 
-        if (ec) {
+        if (ec)
+        {
             std::cout << "could not create connection because: " << ec.message() << std::endl;
             return 0;
         }
@@ -69,11 +99,14 @@ int main(int argc, char* argv[]) {
         // con->send("hey from c++", websocketpp::frame::opcode::text);
 
         // start polling both client and server
-        while (true) {
+        while (true)
+        {
             c.poll();
             websocket_server.poll();
         }
-    } catch (websocketpp::exception const & e) {
+    }
+    catch (websocketpp::exception const & e)
+    {
         std::cout << e.what() << std::endl;
     }
 }
